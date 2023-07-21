751a1a3fd332346cef112ffddef24a56abaa18b3
LissaSobol <sobolenok001@gmail.com>
2023-07-21 21:39:40 +0300

    lesson 4

--- lesson 4/advertisements/app_advertisements/urls.py
+++ lesson 4/advertisements/app_advertisements/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ind
+from .views import index, top_sellers, advertisement_post
 
 urlpatterns = [
-    path("",ind)
+    path("",index, name= "main-page"),
+    path("top-sellers/", top_sellers, name= "top-sellers"),
+    path("advertisement-post", advertisement_post, name= "advertisement-post")
+    
 ]

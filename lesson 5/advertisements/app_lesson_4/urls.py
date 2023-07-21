cd88318f3340e87097fc9be5ec69bc2459e28848
LissaSobol <sobolenok001@gmail.com>
2023-07-21 11:48:59 +0300

    remove lesson 5 from repository

--- lesson 5/advertisements/app_lesson_4/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ind
-
-urlpatterns = [
-    path("",ind)
-]
--- lesson 5/advertisements/app_lesson_4/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def ind(request):
-    return HttpResponse("Домашка по 4 занятию")
-
-# Create your views here.

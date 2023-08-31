3f0e13b9b6e418c1d59e4b69fce1bb0627438530
LissaSobol <sobolenok001@gmail.com>
2023-08-31 19:20:51 +0300

    lesson 11

---
+++ lesson 11/advertisements/app_advertisements/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from .models import Advertisement
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    
+    list_display=[
+        "title", "description", "price", "auction", 
+        "creation_date", "updation_date", "image"]
+   
+   
+    actions = ["set_auction_as_false", "set_auction_as_true"]
+    list_filter = ['price']
+    fieldsets = (
+        ('Общее', {
+            'fields':('title', 'description', 'image'),
+            'classes':['collapse']
+        }),
+        ('Финансы', {
+            'fields' : ('price', 'auction'),
+            'classes':['collapse']
+
+        })
+    )
+
+    @admin.action(description = "Убрать возможность торга")
+    def set_auction_as_false(self, request, queryset):
+        queryset.update(auction = False)
+
+
+    @admin.action(description = "Добавить возможность торга")
+    def set_auction_as_true(self, request, queryset):
+        queryset.update(auction = True)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+
+
+# Register your models here.
